ee5f1c4fd050abda1a16552552f36f9a3f9c09fd
Jackson Hardaker <jackson.hardaker@gmail.com>
2019-12-01 18:27:17 -0500

    fix: filter empty arry items and add comments

--- day-1/index.js
+++ day-1/index.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const calculate = require('./tyranny-of-the-rocket');
 
+// Load data from txt file
 const data = fs.readFileSync(path.join(__dirname, 'data.txt'), {
   encoding: 'utf8'
 });
 
-const dataArray = data.split('\n');
+// Split data file on newline, and filter out any empty entries
+const dataArray = data.split('\n').filter(mass => !!mass);
 
+// Reduce array of masses to single value, buy calculating the fuel for each and summing them.
 const totalFuelRequired = dataArray.reduce((total, mass) => total + calculate(mass), 0);
 
 console.log(totalFuelRequired);
