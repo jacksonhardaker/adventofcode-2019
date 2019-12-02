4cf4626707792b1ad495487ea78c03fa1e9c0893
Jackson Hardaker <jackson.hardaker@gmail.com>
2019-12-01 21:17:20 -0500

    test: add intermediary variables for clarity

--- day-1/__tests__/part-1.test.js
+++ day-1/__tests__/part-1.test.js
@@ -3,19 +3,23 @@ import { calculate } from '../tyranny-of-the-rocket';
 describe('Day 1: The Tyranny of the Rocket Equation - Part 2', () => {
 
   test('for a mass of 12, the result is 2', () => {
-    expect(calculate(12)).toEqual(2);
+    const fuelRequired = calculate(12);
+    expect(fuelRequired).toEqual(2);
   });
   
   test('for a mass of 14, the result is 2', () => {
-    expect(calculate(14)).toEqual(2);
+    const fuelRequired = calculate(14);
+    expect(fuelRequired).toEqual(2);
   });
-
+  
   test('for a mass of 1969, the result is 654', () => {
-    expect(calculate(1969)).toEqual(654);
+    const fuelRequired = calculate(1969);
+    expect(fuelRequired).toEqual(654);
   });
   
   test('for a mass of 100756, the result is 33583', () => {
-    expect(calculate(100756)).toEqual(33583);
+    const fuelRequired = calculate(100756);
+    expect(fuelRequired).toEqual(33583);
   });
 
 });
--- day-1/__tests__/part-2.test.js
+++ day-1/__tests__/part-2.test.js
@@ -3,17 +3,20 @@ import { calculateRecursive } from '../tyranny-of-the-rocket';
 describe('Day 1: The Tyranny of the Rocket Equation - Part 2', () => {
 
   test('for a mass of 12, the result is 2', () => {
-    expect(calculateRecursive(12)).toEqual(2);
+    const fuelRequired = calculateRecursive(12);
+    expect(fuelRequired).toEqual(2);
   });
 
   test('for a mass of 1969, the result is 966', () => {
+    const fuelRequired = calculateRecursive(1969);
     //654 + 216 + 70 + 21 + 5 = 966
-    expect(calculateRecursive(1969)).toEqual(966);
+    expect(fuelRequired).toEqual(966);
   });
 
   test('for a mass of 100756, the result is 50346', () => {
+    const fuelRequired = calculateRecursive(100756);
     //33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346
-    expect(calculateRecursive(100756)).toEqual(50346);
+    expect(fuelRequired).toEqual(50346);
   });
 
 });
